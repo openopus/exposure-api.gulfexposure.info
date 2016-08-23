bd8c7543fd01798a250648a3faa88184f0145f45
Brian J. Fox <bfox@opuslogica.com>
2016-08-23 16:56:58 -0700

    hating

--- app/controllers/codename_controller.rb
+++ app/controllers/codename_controller.rb
@@ -2,8 +2,11 @@
 # 
 #  Copyright (c) 2016 Brian J. Fox
 #  Author: Brian J. Fox (bfox@opuslogica.com) Tue Aug 23 16:20:31 2016.
-class CodenameController < ApplicationController
+class CodenameController < GenericApiRails::RestController
   before_action :setup
+  def model
+    @model ||= User
+  end
   
   def regen
     @user.regen_codename
