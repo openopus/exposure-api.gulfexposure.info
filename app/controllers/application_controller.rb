5a2426aa4d23545aa9e1dd45b9abe3f56909a2f9
Brian J. Fox <bfox@opuslogica.com>
2016-08-24 15:27:02 -0700

    some admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
+  before_action :get_device_id
+
+  private
+  def get_device_id
+    @oli_device_id = request.headers["OLI-Device-ID"]
+  end
 end
